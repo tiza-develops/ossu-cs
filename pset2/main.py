3db8e2068ccf924c12bc7e900549e08368c718ae
tiza <tizadevel@tuta.io>
2024-02-28 17:22:24 -0600

    Now there are various abstractions and there is a for loop under way to compare both strings

--- pset2/main.py
+++ pset2/main.py
@@ -1,29 +1,33 @@
 import linecache
+import itertools
 from random import randint
 from states import *
+from validate import *
 
 # Create the needed new word 
 word = list(linecache.getline(('words.txt'), randint(1, 55900)).strip())
-slots, slot = '_ ' * len(word) , list('_' * len(word))
+slot = '_' * len(word)
 hanged = state0
 ############################################################
 # Here they end the static values, and now we will compare #
 ############################################################
 
 def guess():
-	input("Enter a single letter: ")
+	guess = input("Please enter a letter: ")
+	if valid(guess) == True:
+		return guess
+	else:
+		return print("you need to enter a single case letter")
 
 def destiny():
 	lucky = guess()
-	if lucky in word:
-		# Print the newfound letters
-	else: 
-		# Punish
+	for (i,j) in zip(word,slot):
+		if lucky == i:
+			slot[j] == lucky
+		else:
+			pass
 
-def main:
-	if slot == word:
-		print('yay, you have won')
-	else:
-		destiny()
+def main():
+	destiny()
 
-
+main()
---
+++ pset2/validate.py
@@ -0,0 +1,5 @@
+def valid(a):
+	if len(a) == 1 and a.isalpha():
+		return True
+	else:
+		return False
