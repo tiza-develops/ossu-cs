f98aef4b613feff1ddf2a32399b64339bb7c0b14
tiza <tizadevel@tuta.io>
2024-03-03 08:40:38 -0600

    now global variables in local functions

--- pset2/src/main.py
+++ pset2/src/main.py
@@ -1,34 +1,42 @@
 import linecache
 import sys
+from states import *
 from random import randint
 from control import *
 
-# Create the needed new word 
+# Create the needed new word
 
-word = list(linecache.getline(('../resources/words.txt'), randint(1, 55900)).strip())
+word = list(linecache.getline(
+    ('../resources/words.txt'), randint(1, 55900)).strip())
 slot = list('_' * len(word))
-
+hanged = state0
+number = 0
 ############################################################
 # Here they end the static values, and now we will compare #
 ############################################################
 
+
 def destiny(a):
-        for i in range(len(word)):
-            if a == word[i]:
-                slot[i] = a
-            else:
-                pass         
-        print(''.join(slot))
+    for i in range(len(word)):
+        if a == word[i]:
+            slot[i] = a
+        else:
+            pass
+
+    print(''.join(slot))
+
 
 def main():
-	while word != slot:
-		a = input("$: ")
-		valid(a)
-		if valid(a) == True:
-		    destiny(a)
-		else:
-			print("you should provide a single letter")
-	else:
-		print("you have won!")
-		sys.exit(0)
+    while word != slot:
+        a = input("$: ")
+        valid(a)
+        if valid(a) == True:
+            destiny(a)
+        else:
+            print("you should provide a single letter")
+    else:
+        print("you have won!")
+        sys.exit(0)
+
+
 main()
--- pset2/src/test.py
+++ pset2/src/test.py
@@ -1,1 +1,8 @@
-
+state = 0
+def punish():
+	global state
+	hanged = state
+	hanged += 1
+	state = hanged
+	print(state)
+punish()
